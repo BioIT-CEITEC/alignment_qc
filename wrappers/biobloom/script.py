49a4bc7bbe050ee5827afcb9039dd9c0a202bef2
Niko <245957@muni.cz>
2022-07-09 12:54:31 +0200

    fix biobloom species

--- wrappers/biobloom/script.py
+++ wrappers/biobloom/script.py
@@ -32,24 +32,24 @@ if float(mapped_reads) > snakemake.params.max_mapped_reads_to_run:
     # set up contamination filters
     human_38 = ref_dir + "homo_sapiens/GRCh38-p10/index/BioBloomTools/human_38.bf"
     mouse    = ref_dir + "mus_musculus/GRCm38.p6-93/index/BioBloomTools/mouse.bf"
-    rat      = ref_dir + "rattus_norvegicus/Rnor_6.0-91/index/BioBloomTools/rat.bf"
+    #rat      = ref_dir + "rattus_norvegicus/Rnor_6.0-91/index/BioBloomTools/rat.bf"
     yeast    = ref_dir + "saccharomyces_cerevisiae/R64-1-1.100/index/BioBloomTools/yeast.bf"
     fruit_fly= ref_dir + "drosophila_melanogaster/BDGP6-99/index/BioBloomTools/fruit_fly.bf"
-    dog      = ref_dir + "canis_familiaris/CanFam3.1-101/index/BioBloomTools/dog.bf"
+    #dog      = ref_dir + "canis_familiaris/CanFam3.1-101/index/BioBloomTools/dog.bf"
     athaliana= ref_dir + "arabidopsis/TAIR10-31/index/BioBloomTools/A.thaliana.bf"
-    brassica = ref_dir + "brassica_napus/Bra_napus_v2/index/BioBloomTools/B.napus.bf"
+    #brassica = ref_dir + "brassica_napus/Bra_napus_v2/index/BioBloomTools/B.napus.bf"
     c_elegans= ref_dir + "c_elegans/WBcel235-102/index/BioBloomTools/C.elegans.bf"
 
     filters_list = list()
     if snakemake.params.filters == "all":
         filters_list.append(human_38)
         filters_list.append(mouse)
-        filters_list.append(rat)
+        #filters_list.append(rat)
         filters_list.append(yeast)
         filters_list.append(fruit_fly)
-        filters_list.append(dog)
+        #filters_list.append(dog)
         filters_list.append(athaliana)
-        filters_list.append(brassica)
+        #filters_list.append(brassica)
         filters_list.append(c_elegans)
     else:
         for f in filters.split(';'):
@@ -57,18 +57,18 @@ if float(mapped_reads) > snakemake.params.max_mapped_reads_to_run:
                 filters_list.append(human_38)
             elif f == 'mouse':
                 filters_list.append(mouse)
-            elif f == 'rat':
-                filters_list.append(rat)
+            # elif f == 'rat':
+            #     filters_list.append(rat)
             elif f == 'yeast':
                 filters_list.append(yeast)
             elif f == 'fly':
                 filters_list.append(fruit_fly)
-            elif f == 'dog':
-                filters_list.append(dog)
+            # elif f == 'dog':
+            #     filters_list.append(dog)
             elif f == 'arabidopsis':
                 filters_list.append(athaliana)
-            elif f == 'brassica':
-                filters_list.append(brassica)
+            # elif f == 'brassica':
+            #     filters_list.append(brassica)
             elif f == 'c_elegans':
                 filters_list.append(c_elegans)
             else:
